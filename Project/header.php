b160c97f1345df0d1216304d51d2bd79904a2675
Nuno Ramos <nunor07.unfc@gmail.com>
2016-11-25 21:42:17 +0000

    starting with login

--- Project/header.php
+++ Project/header.php
@@ -3,6 +3,13 @@
     <head>
         <title>My Restaurant</title>
         <meta charset="utf-8">
+        <form action="index.php" method="post">
+            username:
+            <input type="text" name="username">
+            password:
+            <input type="text" name="password">
+            <input type="submit" value="login">
+        </form>
         <h4>The best restaurant website!!</h4>
     </head>
     <body>
--- Project/index.php
+++ Project/index.php
@@ -4,4 +4,12 @@ include_once('header.php');
 include_once ('mainPageContent.php');
 
 include_once('footer.php');
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+$db = new PDO('sqlite:database/database.db');
+
+$stmt = $db->prepare('SELECT * FROM user');
+$stmt->execute();
+$result = $stmt->fetchAll();
 ?>
