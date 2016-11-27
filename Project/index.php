f225da6a3ca328e487148ac24fd493cfc7fa0d02
Nuno Ramos <nunor07.unfc@gmail.com>
2016-11-27 23:09:19 +0000

    Continuing to organize

--- Project/index.php
+++ Project/index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-$db = new PDO('sqlite:database/database.db');
 $_SESSION['incorrectLogin_flag']=0;
 include_once('templates/header.php');
 
 include_once ('mainPageContent.php');
 
 include_once('templates/footer.php');
+
 ?>
