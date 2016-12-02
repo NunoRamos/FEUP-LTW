f44258122f7ba4e25b10b0115ebc26ce13bb830d
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2016-12-02 11:32:57 +0000

    CSS - Sign Up

--- Project/pages/signup.php
+++ Project/pages/signup.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Log-in</title>
+    <title>Sign Up</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="../css/reset.css" type="text/css">
     <link rel="stylesheet" href="../css/signup.css" type="text/css">
+    <link rel="stylesheet" href="../css/footer.css" type="text/css">
 </head>
 <body>
 <div>
@@ -16,23 +17,58 @@
 }
     ?>
 </div>
+<div id="logoImage">
+  <img src="../images/logo.jpg" alt="logo" height="200px">
+</div>
 <div id="userNameSU">
     <form action="actions/signup.php" method="post">
-        Username<br>
-        <input type="text" name="username" required><br>
-        Password<br>
-        <input type="password" name="password" required><br>
-        Confirm Password<br>
-        <input type="password" name="confirmPassword" required><br>
-        Name<br>
-        <input type="text" name="name" required><br>
-        Email<br>
-        <input type="e-mail" name="email" required><br>
-        Gender<br>
+        <label>
+          <br>
+          Username
+          <br>
+        <input type="text" name="username" required>
+        <br>
+      </label>
+      <label>
+        <br>
+        Password
+        <br>
+        <input type="password" name="password" required>
+        <br>
+      </label>
+      <label>
+        <br>
+        Confirm Password
+        <br>
+        <input type="password" name="confirmPassword" required>
+        <br>
+      </label>
+      <label>
+        <br>
+        Name
+        <br>
+        <input type="text" name="name" required>
+        <br>
+      </label>
+      <label>
+        <br>
+        Email
+        <br>
+        <input type="e-mail" name="email" required>
+        <br>
+      </label>
+      <label>
+        <br>
+        Gender
+        <br>
         <select name="gender">
             <option value="male">Male</option>
             <option value="Female">Female</option>
-        </select><br>
+        </select>
+        <br>
+      </label>
+      <br>
+      <br>
         <input type="submit" value="Create Account">
     </form>
 </div>
