95f6d4c13d81ba8cab3d161c221ba85e64ca4040
Nuno Ramos <nunor07.unfc@gmail.com>
2016-12-05 16:56:11 +0000

    Moving search box to the body of the main page

--- Project/pages/templates/header.php
+++ Project/pages/templates/header.php
@@ -26,15 +26,6 @@ session_start();
 
         }
         ?>
-        <h4>The best restaurant website!!</h4>
-        <?php
-        echo '
-        <form action="actions/restaurantSearch.php" method="post">
-            <input required type="text" name="search" placeholder="Search Restaurants">
-            <input type="submit" value="search">
-        </form>
-        ';
-        ?>
 
 
     </head>
--- Project/pages/templates/mainPageContent.php
+++ Project/pages/templates/mainPageContent.php
@@ -3,3 +3,11 @@
   <input type="text" name="search" value="Search Restaurants">
   <input type="submit" value="search">
 </form-->
+
+<h4>The best restaurant website!!</h4>
+<?php
+echo '<form action="actions/restaurantSearch.php" method="post">
+        <input required type="text" name="search" placeholder="Search Restaurants">
+        <input type="submit" value="search">
+        </form>';
+?>
