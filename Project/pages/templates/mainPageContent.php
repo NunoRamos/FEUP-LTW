6aac673d1cae50bcb67a9f4378dfbf93ffa7169c
Nuno Ramos <nunor07.unfc@gmail.com>
2016-12-05 19:36:41 +0000

    Fixing a bug

--- Project/pages/templates/mainPageContent.php
+++ Project/pages/templates/mainPageContent.php
@@ -2,11 +2,10 @@
 <div class="relative">
     <h4 id="mainPageTitle">The best restaurant website!!</h4>
 </div>
-<?php
-    echo '<div id="searchBox">
-            <form action="actions/restaurantSearch.php" method="post">
-            <input id="searchBoxStyle" required type="text" name="Search" placeholder="Search Restaurants">
-            <input id="searchBoxSubmit" type="submit" value="search">
-            </form>
-          </div>';
-?>
+<div id="searchBox">
+   <form action="actions/restaurantSearch.php" method="post">
+       <input id="searchBoxStyle" required type="text" name="search" placeholder="Search Restaurants">
+       <input id="searchBoxSubmit" type="submit" value="Search">
+   </form>
+</div>
+
