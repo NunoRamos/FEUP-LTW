4eeaeccd64977eefe390d189b6570f0d83f77aa1
Inês Teixeira <inescfteixeira@gmail.com>
2016-12-11 18:34:54 +0000

    Search by types added

--- Project/pages/templates/mainPageContent.php
+++ Project/pages/templates/mainPageContent.php
@@ -20,14 +20,38 @@
         <input id="searchBoxSubmit" class="buttonStyle" type="submit" value="Search">
       </form>
     <div id="iconsTypes">
+
+        <a href="../pages/actions/restaurantSearch.php?search=Cafe">
       <img id="iconPequenoAlmoco" src="../images/icons/pequenoAlmoco.png" alt="Pequeno Almoço" height="100px">
-      <img id="iconAlmoco" src="../images/icons/almoco.png" alt="Almoço" height="100px">
+        </a>
+
+        <a href="../pages/actions/restaurantSearch.php?search=Italiano">
+            <img id="iconAlmoco" src="../images/icons/almoco.png" alt="Almoço" height="100px">
+        </a>
+
+        <a href="../pages/actions/restaurantSearch.php?search=Churrasqueira">
       <img id="iconJantar" src="../images/icons/jantar.png" alt="Jantar" height="100px">
-      <img id="iconEntregas" src="../images/icons/entregas.png" alt="Entregas" height="100px">
+        </a>
+
+        <a href="../pages/actions/restaurantSearch.php?search=Francesinha">
+      <img id="iconLoaf" src="../images/icons/loaf.png" alt="Francesinha" height="100px">
+        </a>
+
+        <a href="../pages/actions/restaurantSearch.php?search=Pub">
       <img id="iconBeberUmCopo" src="../images/icons/beberUmCopo.png" alt="Beber Um Copo" height="100px">
+        </a>
+
+        <a href="../pages/actions/restaurantSearch.php?search=Pastelaria">
       <img id="iconCafesEPastelarias" src="../images/icons/cafesEPastelarias.png" alt="Cafés E Pastelarias" height="100px">
-      <img id="iconTakeAway" src="../images/icons/takeAway.png" alt="Take Away" height="100px">
+        </a>
+
+        <a href="../pages/actions/restaurantSearch.php?search=Takeway">
+        <img id="iconTakeAway" src="../images/icons/takeAway.png" alt="Take Away" height="100px">
+        </a>
+
+        <a href="../pages/actions/restaurantSearch.php?search=Gourmet">
       <img id="iconRefeicoesDeLuxo" src="../images/icons/refeicoesDeLuxo.png" alt="Refeições De Luxo" height="100px">
+        </a>
     </div>
   </div>
 </div>
