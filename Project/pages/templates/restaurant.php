82eeb08acabd9ee1422e88aab46d89882de49c6b
Inês Teixeira <inescfteixeira@gmail.com>
2016-12-14 10:54:56 +0000

    fixing a bug

--- Project/pages/templates/restaurant.php
+++ Project/pages/templates/restaurant.php
@@ -10,7 +10,7 @@ $_SESSION['token'] = generateRandomToken();
     <link rel="stylesheet" href="../css/design.css" type="text/css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script src="/js/restaurant.js"></script>
+    <script src="../js/restaurant.js"></script>
 
     <div class="pageContent">
         <div class="design">
